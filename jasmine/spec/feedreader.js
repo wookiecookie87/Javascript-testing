84342016d0a6bbfdee46a528a9c1f64538950afc
Wookiecookie <wookiecookie87@gmail.com>
2016-01-18 00:33:05 +0900

    Edits

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,42 +31,92 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('are defined and not empty', function(){
+            allFeeds.forEach(function(feed){
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBeNull();
+            });
+         })
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+         it('are defined and not empty', function(){
+            allFeeds.forEach(function(feed){
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBeNull();
+            });
+         })
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe("The menu", function(){
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+        it('is hidden by default', function(){
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it('is toggling on click', function(){
+            var classExist = $('body').hasClass('menu-hidden');
+            $('.menu-icon-link').click();
+            var newClassExist = $('body').hasClass('menu-hidden');
+            expect(newClassExist).not.toEqual(classExist);
+            $('.menu-icon-link').click();
+            classExist = $('body').hasClass('menu-hidden');
+            expect(newClassExist).not.toEqual(classExist);
+
+        })
+    });      
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+   describe("Initial Entries", function(){
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         beforeEach(function(done){
+             loadFeed(0, function(){
+                done();
+             });
+         });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
-
+         it('gets ajax data', function(done) {
+            var len = $('.entry').length;
+            expect(len).toBeGreaterThan(0);
+            done();
+         });
+    });
+    /* TODO: Write a new test suite named "New Feed Selection"*/
+   describe("New Feed Selection", function(){
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        beforeEach(function(done){
+            loadFeed(0, function(){
+            done();
+            });
+        });
+
+        it('changes data', function(done) {
+            var original = $('.entry');
+        
+            
+            done();
+        });
+    });    
 }());
--- js/app.js
+++ js/app.js
@@ -43,7 +43,6 @@ function init() {
  function loadFeed(id, cb) {
      var feedUrl = allFeeds[id].url,
          feedName = allFeeds[id].name;
-
      $.ajax({
        type: "POST",
        url: 'https://rsstojson.udacity.com/parseFeed',
@@ -68,7 +67,7 @@ function init() {
                  entries.forEach(function(entry) {
                      container.append(entryTemplate(entry));
                  });
-
+                 
                  if (cb) {
                      cb();
                  }
